d03c6c6528a2b84140bff05f9c7c6ef164e0b37b
Daniel Tore <danieltore@gmail.com>
2020-11-23 10:01:57 +0100

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
     console.log('usando BBDD local');
 } else {
-    urlDB = 'mongodb+srv://Admin:xYkeBxsIvTOVSD7D@cluster0.x0lnk.mongodb.net/cafe?retryWrites=true&w=majority'
+    urlDB = process.env.MONGO_URI
     console.log('usando BBDD en Mongo - Atlas');
 }
 process.env.URLDB = urlDB
